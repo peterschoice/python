e2c2b9a2131c8218075d386e786d0855285d1e94
peterschoice <peterschoice@gmail.com>
2019-12-18 15:06:39 +0900

    조건

---
+++ 04_example.py
@@ -0,0 +1,34 @@
+# print() happy hacking!
+
+# 일반변수 
+
+hphk = "해피해킹"
+
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+
+
+menus1 = "순남"
+menus2 = "양자강"
+menus3 = "20층..."
+
+# List 
+menus = [menus1,menus2,menus3]
+
+print(menus)
+print(menus[0])
+
+
+# Distionary 
+
+dict_nums = { "순남 시래기" : "02-6589-3652", "양자강" : "02-6589-3652" , "20" : "02-6589-3652" }
+
+print(dict_nums.get("순남"))
+
+print(dict_nums["순남 시래기"])
+
+
+
+
